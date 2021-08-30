f9050629a54e9756ef3456b7c10058fbac5e4a74
parix1999 <parisottoedoardo@gmail.com>
2021-08-30 15:06:13 +0200

    milestone-1

--- index-prima-milestone.php
+++ index-prima-milestone.php
@@ -6,17 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Album</title>
     <link rel="stylesheet" href="style-uno.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
     
     <?php
-
+        /*
+        Prima Milestone:
+        Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: 
+        al caricamento della pagina ci saranno tutti i dischi. => index-prima-milestone.php
+        */
         require_once __DIR__ . "/database/data.php";
-        foreach($database as $key => $item) {
-            echo "{$item['title']} <br>";
-        }
+        // foreach($database as $key => $item) {
+        //     echo "{$item['title']} <br>";
+        // }
     ?>
 
+    <?php require_once __DIR__ . "/pezzi/header.php"; ?>
+
+    
+
+
+    
 
 </body>
 </html>
---
+++ pezzi/header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="container-head">
+        <div class="box-logo">
+            <i class="fab fa-spotify"></i>
+        </div>
+
+    </div>
+</header> 
