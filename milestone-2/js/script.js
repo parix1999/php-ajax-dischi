ab6a7e877bfb3e50c54e79dea18555528fbbb7f4
parix1999 <parisottoedoardo@gmail.com>
2021-08-30 17:42:42 +0200

    milestone-2

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -4,10 +4,13 @@ new Vue (
     {
         el:'#app',
         data: {
-
+            albums: [],
         },
         created() {
-            
+            axios.get('http://localhost:8888/php-ajax-dischi/milestone-2/api/api.php')
+            .then((response) => {
+                this.albums = response.data;
+            });
         }
 
     }
