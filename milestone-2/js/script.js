ff606786e534f5a713064a76c186ec039a80cfcd
parix1999 <parisottoedoardo@gmail.com>
2021-08-31 18:09:57 +0200

    milestone-2-b

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -27,7 +27,8 @@ new Vue (
             clicca: function(){
                     axios.get(`http://localhost:8888/php-ajax-dischi/milestone-2/api/filtered.php?genere=${this.selected}`)
                     .then((response) => {
-                        if(this.filtrato.length <= 0) {
+                        console.log('cliccato');
+                        if(this.selected.length === 0) {
                             this.filtrato = this.albums;
                         }
                         this.filtrato = response.data;
