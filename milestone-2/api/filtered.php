30eae7453c68e7fba682044bd76bd7339d5b35ac
parix1999 <parisottoedoardo@gmail.com>
2021-08-31 12:01:57 +0200

    mileston-2-search

---
+++ milestone-2/api/filtered.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . "/../database/data.php";
+require_once __DIR__ . "/../functions/type-api.php";
+
+//var_dump($database);
+// Qui in mezzo deve essererci il filtro:
+$genere = $_GET['genere'];
+$filtro = array_filter($database, function($v) use ($genere) {
+    if ($v['genre'] === $genere) {
+        return true;
+    } else {
+        return false;
+    }
+    
+
+});
+var_dump($filtro);
+
+// Trasformazione nell'api in json filtered:
+createJson($database);
