644c1a87923e109133d4969cb01a2b539db638a6
parix1999 <parisottoedoardo@gmail.com>
2021-08-31 14:54:22 +0200

    milestone-2-b

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -7,21 +7,32 @@ new Vue (
             albums: [],
             filtrato: [],
             // Select:
-            selected: '',
+            selected: ''
             
         },
         created() {
             axios.get('http://localhost:8888/php-ajax-dischi/milestone-2/api/api.php')
             .then((response) => {
                 this.albums = response.data;
-            });
+            }); 
 
-            axios.get('http://localhost:8888/php-ajax-dischi/milestone-2/api/filtered.php?genere=Rock')
-            .then((response) => {
-                this.filtrato = this.albums;
-                this.filtrato = response.data; 
-                console.log(this.filtrato);
-            });
+            // axios.get(`http://localhost:8888/php-ajax-dischi/milestone-2/api/filtered.php?genere=${this.selected}`)
+            // .then((response) => {
+            //     this.filtrato = this.albums;
+            //     console.log(this.filtrato);
+            // });
+            
+        },
+        methods: {
+            clicca: function(){
+                    axios.get(`http://localhost:8888/php-ajax-dischi/milestone-2/api/filtered.php?genere=${this.selected}`)
+                    .then((response) => {
+                        this.filtrato = this.albums;
+                        this.filtrato = response.data;
+                        console.log(this.filtrato); 
+                    
+                    });
+            }
         }
 
     }
